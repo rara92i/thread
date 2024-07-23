336ce85fbd74863552640a6f3d177132db8d9f8f
rara92i <rpmarketingmedia@gmail.com>
2024-07-23 16:41:18 +0200

    change nav

--- app/components/DashboardNav.tsx
+++ app/components/DashboardNav.tsx
@@ -1,18 +1,18 @@
 import { Button } from "@/components/ui/button"
-import { signOut } from "firebase/auth"
-import { useRouter } from "next/navigation"
-import { auth } from "@/db/firebaseConfig"
+// import { signOut } from "firebase/auth"
+// import { useRouter } from "next/navigation"
+// import { auth } from "@/db/firebaseConfig"
 import { LogOut,SquarePlus,Settings   } from 'lucide-react';
 import Link from "next/link"
 
 export default function DashboardNav() {
 
-  const router = useRouter()
+  // const router = useRouter()
 
-  const handleSignOut = ()=> {
-    signOut(auth)
-    router.push('/')
-  }
+  // const handleSignOut = ()=> {
+  //   signOut(auth)
+  //   router.push('/')
+  // }
 
   return (
     <div className='flex items-center justify-between mb-4'>
@@ -29,9 +29,9 @@ export default function DashboardNav() {
       </Link>
         
       </div>    
-      <Button onClick={handleSignOut}>
+      {/* <Button onClick={handleSignOut}>
         <LogOut className="w-4"/>
-      </Button>
+      </Button> */}
        
     </div>
   )
--- app/components/Nav.tsx
+++ app/components/Nav.tsx
@@ -1,10 +1,26 @@
+'use client';
+
 import LogoMdc from "@/public/logo1.svg";
 import Image from "next/image";
 import Link from "next/link"
-import { User } from 'lucide-react';
+import { useRouter } from "next/navigation"
+import { User, LogOut } from 'lucide-react';
 import { Button } from "@/components/ui/button";
+import  useAuth  from "@/hooks/useAuth";
+import { signOut } from "firebase/auth"
+import { auth } from "@/db/firebaseConfig"
+
 
 export default function Nav() {
+
+  const {user} = useAuth()
+  const router = useRouter()
+  
+  const handleSignOut = ()=> {
+    signOut(auth)
+    router.push('/')
+  }
+
   return (
     <nav className="max-w-[1200px] w-full mx-auto h-[80px] flex items-center justify-between p-5 border-b  border-gray-300">
 
@@ -15,11 +31,23 @@ export default function Nav() {
       </div>
 
       <div className="flex items-center gap-4">
-          <Link href="/signInAndUp">
-            <Button>
-                <User className="w-4" />
-            </Button>
-          </Link>
+        {!user ?(
+        <Link href="/signInAndUp">
+        <Button className="bg-blue-950">
+          <User className="w-4" />
+        </Button>
+        </Link> ) : (
+          <>
+          <Link href="/dashboard">
+        <Button>
+          <User className="w-4" />
+        </Button>
+        </Link> 
+        <Button onClick={handleSignOut}>
+            <LogOut className="w-4" />
+          </Button>
+          </>
+        )}
       </div>
 
   </nav>
